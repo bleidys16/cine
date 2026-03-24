4eb1e2d353a4caa103b5002ac00655f7007a772a
cervantessaralucia4-puntocom <cervantessaralucia4@gmail.com>
2026-03-24 15:34:14 -0500

    camilo arreglo el link del backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://cine-41x4.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
